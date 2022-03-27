67497723bad1a652474ad072d588e8bd67880106
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-03-27 15:54:50 +0530

    new approach

--- src/Patterns/Pattern_5.java
+++ src/Patterns/Pattern_5.java
@@ -12,32 +12,55 @@ public class Pattern_5 {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number : ");
         int n = sc.nextInt();
-        n = n /2 +1;
-        for (int i = 1; i <= n; i++) {
-            for(int j=1; j <= n-i; j++){
+//        n = n /2 +1;
+//        for (int i = 1; i <= n; i++) {
+//            for(int j=1; j <= n-i; j++){
+//                System.out.print("\t");
+//            }
+//            for(int j=1; j <= i; j++){
+//                System.out.print("*\t");
+//            }
+//
+//            for(int j=1; j < i; j++){
+//                System.out.print("*\t");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = n-1; i >=1 ; i--) {
+//            for(int j = 1; j <= n-i; j++ ){
+//                System.out.print("\t");
+//            }
+//            for(int j=1; j <= i; j++){
+//                System.out.print("*\t");
+//            }
+//            for(int j=1; j < i; j++){
+//                System.out.print("*\t");
+//            }
+//
+//            System.out.println();
+//        }
+
+//        New approch
+        int sp = n/2;
+        int st = 1;
+        for(int i=1; i<= n; i++){
+            for(int j=1; j <= sp; j++){
                 System.out.print("\t");
             }
-            for(int j=1; j <= i; j++){
+            for(int j=1; j<= st; j++){
                 System.out.print("*\t");
             }
-
-            for(int j=1; j < i; j++){
-                System.out.print("*\t");
+            if (i <= n / 2) {
+                st +=2;
+                sp--;
+            }
+            else{
+                st -=2;
+                sp++;
             }
             System.out.println();
         }
-        for (int i = n-1; i >=1 ; i--) {
-            for(int j = 1; j <= n-i; j++ ){
-                System.out.print("\t");
-            }
-            for(int j=1; j <= i; j++){
-                System.out.print("*\t");
-            }
-            for(int j=1; j < i; j++){
-                System.out.print("*\t");
-            }
 
-            System.out.println();
-        }
+
     }
 }
