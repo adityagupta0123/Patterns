9e0e129048f758b2098b24eec17298533812183b
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-03-29 23:42:40 +0530

    create Pattern_8

---
+++ src/Patterns/Pattern_8.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number : ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1 ; i--) {
+            for (int j = 1; j <= i ; j++) {
+                if (i == j)
+                    System.out.print("*\t");
+                else
+                    System.out.print("\t");
+
+            }
+            System.out.println();
+        }
+    }
+}
