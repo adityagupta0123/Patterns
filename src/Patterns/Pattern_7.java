48729b7eea1c542a36db2a68c7ff9fa86dcc22df
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-03-29 23:27:51 +0530

    create Pattern_7

---
+++ src/Patterns/Pattern_7.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_7 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("enter number : ");
+        int n = sc.nextInt();
+        int st = 1;
+        for (int i = 1; i <= n ; i++) {
+            for (int j=1; j<=i; j++)
+                System.out.print("\t");
+            for (int j = 1; j <=st ; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
