aa0c104f775651225a165fdff279277b8539801a
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-03-29 23:36:45 +0530

    new approach

--- src/Patterns/Pattern_7.java
+++ src/Patterns/Pattern_7.java
@@ -7,12 +7,18 @@ public class Pattern_7 {
         Scanner sc  = new Scanner(System.in);
         System.out.println("enter number : ");
         int n = sc.nextInt();
-        int st = 1;
+//        int st = 1;
         for (int i = 1; i <= n ; i++) {
-            for (int j=1; j<=i; j++)
-                System.out.print("\t");
-            for (int j = 1; j <=st ; j++) {
-                System.out.print("*\t");
+//            for (int j=1; j<=i; j++)
+//                System.out.print("\t");
+//            for (int j = 1; j <=st ; j++) {
+//                System.out.print("*\t");
+//            }
+            for (int j = 1; j <=i ; j++) {
+                if (i == j)
+                    System.out.print("*\t");
+                else
+                    System.out.print("\t");
             }
             System.out.println();
         }
