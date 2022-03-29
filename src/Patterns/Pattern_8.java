3e1bbeedde38213bdcfdd24251838cf7c167fdd1
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-03-29 23:49:09 +0530

    new approach

--- src/Patterns/Pattern_8.java
+++ src/Patterns/Pattern_8.java
@@ -8,9 +8,19 @@ public class Pattern_8 {
         System.out.println("enter number : ");
         int n = sc.nextInt();
 
-        for (int i = n; i >= 1 ; i--) {
-            for (int j = 1; j <= i ; j++) {
-                if (i == j)
+//        for (int i = n; i >= 1 ; i--) {
+//            for (int j = 1; j <= i ; j++) {
+//                if (i == j)
+//                    System.out.print("*\t");
+//                else
+//                    System.out.print("\t");
+//
+//            }
+//            System.out.println();
+//        }
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if (i + j == n+1)
                     System.out.print("*\t");
                 else
                     System.out.print("\t");
