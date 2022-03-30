ed28fdd9a7f3a0b1b8cbfbea0c55cc82fef905be
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-03-30 23:04:10 +0530

    create Pattern_09

---
+++ src/Patterns/Pattern_09.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number : ");
+        int n = sc.nextInt();
+        for(int i =1 ; i <= n; i++){
+            for (int j = 1; j <=n ; j++) {
+                if (i + j == n+1 || i == j)
+                    System.out.print("*\t");
+                else
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+}
