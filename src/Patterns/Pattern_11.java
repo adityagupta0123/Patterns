913a75478859690aaf13ad51bc0c35fa7ed6d78f
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-03-31 14:46:02 +0530

    create Pattern_11

---
+++ src/Patterns/Pattern_11.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_11 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter Number : ");
+        int n = sc.nextInt();
+        int number = 1;
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(number++ + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
