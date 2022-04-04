f8d7e2830f6a4865a24d34de9eb9d9ba6740ec41
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-04-05 00:05:04 +0530

    change

--- src/Patterns/Pattern_16.java
+++ src/Patterns/Pattern_16.java
@@ -10,14 +10,21 @@ public class Pattern_16 {
         int sp = 2 * n-3;
         int st  = 1 ;
         for(int i=1; i <= n ; i++){
+            int n1=1;
             for (int j = 1; j <= st ; j++) {
-                System.out.print("*\t");
+                System.out.print(n1+"\t");
+                n1++;
             }
             for (int j = 1; j <= sp ; j++) {
                 System.out.print("\t");
             }
+            if( i == n){
+                st--;
+                n--;
+            }
             for (int j = 1; j <= st; j++) {
-                System.out.print("*\t");
+                n1--;
+                System.out.print(n1+"\t");
             }
             st++;
             sp -=2;
