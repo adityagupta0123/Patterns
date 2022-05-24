8eca9be6b3449d852de67655a9e91738cd6beeb8
adityagupta0123 <aditya.gupta3182003@gmail.com>
2022-05-24 17:21:30 +0530

    create Pattern_20.java

---
+++ src/Patterns/Pattern_20.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_20 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("enter no. : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if(j == 1 || j == n){
+                    System.out.print("*\t");
+                }else if(i > n/2 && (i == j || i + j == n+1)) { // for M pattern i <= n/2+1 && ....
+                    System.out.print("*\t");
+                }
+                else
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+}
